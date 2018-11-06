3a5b65e923e0c29e983338a27c39c4d4009c5be8
Banks, Xavier <XBanks@topps.com>
2018-11-05 23:44:18 -0500

    Completed Q3, 5/5, also added some comments.

--- projects/project-3-reinforcement/analysis.py
+++ projects/project-3-reinforcement/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -20,42 +20,75 @@
 # value iteration.
 
 def question2():
+    """
+    Changing the Noise factor to be low/non-existant means that the
+    agent is going to move with complete confidence, it knows that if
+    it makes a move, it will not mis-step and fall into a cliff. So it
+    knows that it can safely risk the cliff.
+    """
     answerDiscount = 0.9
     answerNoise = 0.0
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+    A high discount factor means that the agent is going to value closer
+    rewards far more than later ones, so it goes for the quickest reward it
+    can get. The low/non-existant noise means that there is no reason for the
+    agent to fear the cliff, because it knows that it will not fall off, so it
+    can risk the shorter path.
+    """
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+    The moderate discount reduces the value of later rewards, and the negative
+    living reward forces the agent to end the game quickly. The small amount of
+    noise forces the agent to prefer the safer path.
+    """
+    answerDiscount = 0.5
+    answerNoise = 0.2
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+    The low noise allows the agent to risk the shorter path, and the low discount
+    factor allows it to value later rewards highly, so it goes for the higher value,
+    yet further exit.
+    """
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+    Low discount allows the agent to be patient for high rewards coming later,
+    and the small noise factor makes the agent prefer the safer path.
+    """
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+    The high discount makes the agent prefer taking the shortest path
+    to the closest reward and the noise makes it prefer a safer path,
+    however these are conflicting, so it doesn't know what to do
+    so it just goes back and forth.
+    """
+    answerDiscount = 0.1
+    answerNoise = 0.2
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
